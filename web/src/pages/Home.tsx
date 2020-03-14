887fd628df0a9f0539f00c45f957b12cc1fc9d15
Igor Nehoroshev <mail@neigor.me>
2020-03-14 21:21:18 +0200

    Fix overflow issue

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -9,17 +9,21 @@ const styles = {
     marginLeft: "50%",
     transform: "translateX(-50%)"
   } as CSSProperties,
+  page: {
+    overflow: "hidden",
+    padding: "10px"
+  } as CSSProperties
 };
 
 export class Home extends React.PureComponent {
   render() {
     return (
-      <React.Fragment>
+      <div style={styles.page}>
         <Typography variant="h2" style={styles.title}>
           My SafeHouse
         </Typography>
         <img src="/house.gif" alt="House" style={styles.house} />
-      </React.Fragment>
+      </div>
     );
   }
 }
