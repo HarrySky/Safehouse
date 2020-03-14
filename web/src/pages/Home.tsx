b3bf3489cec8fb2857ccfd6971f3840e5b23f960
Igor Nehoroshev <mail@neigor.me>
2020-03-14 20:06:48 +0200

    Create Home page draft design

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  title: {
+    textAlign: "center"
+  } as CSSProperties,
+  house: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto"
+  } as CSSProperties,
+};
 
 export class Home extends React.PureComponent {
   render() {
     return (
-      <div>Home Page</div>
+      <React.Fragment>
+        <Typography variant="h2" style={styles.title}>
+          My SafeHouse
+        </Typography>
+        <img src="/house.gif" alt="House" style={styles.house} />
+      </React.Fragment>
     );
   }
 }
