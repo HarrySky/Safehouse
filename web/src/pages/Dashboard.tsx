bf63724d88540df615f2fd7f9ccf1058c7954dc9
Animehater <notwtf@gmail.com>
2020-03-15 14:25:05 +0200

    Dashboard changed

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -1,9 +1,115 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Checkbox from '@material-ui/core/Checkbox';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  title: {
+    textAlign: "center",
+    fontFamily: "Lato",
+    fontWeight: 700,
+    fontSize: '32px',
+    color: '#262642',
+    marginTop: '25px',
+    marginBottom: '10px'
+  } as CSSProperties,
+  plaintext:{
+    textAlign: "center",
+    fontFamily: "Lato",
+    fontSize: '32px',
+    marginBottom: '20px'
+  } as CSSProperties,
+  hands: {
+    marginLeft: "50%",
+    transform: "translateX(-50%)",
+    width: '100px',
+    border: 'solid',
+    borderWidth: '1.5px',
+    borderColor:'#262642',
+    borderRadius: '12px',
+    boxShadow: ' 1px 2px 2px 3px #CDC7D7',
+    marginBottom: '10px'
+  } as CSSProperties,
+  page: {
+    overflow: "hidden",
+    padding: "10px"
+  } as CSSProperties,
+  list:{
+    fontFamily: "Lato",
+    fontSize: '32px',
+    marginLeft: "15%",
+    marginRight: '20px',
+  } as CSSProperties
+};
 
 export class Dashboard extends React.PureComponent {
   render() {
     return (
-      <div>Dashboard Page</div>
+      <div>
+      <Typography variant="h2" style={styles.title}>
+        Task Manager
+      </Typography>
+
+      <List style={styles.list}>
+        <ListItem>
+          <IconButton>
+            <img src="/soap.png" alt="Soap"  style={styles.hands}/>
+          </IconButton>
+          <ListItemText
+            disableTypography={true}
+            primary="Personal hygiene">
+              <LinearProgress
+                variant="determinate"
+                color="primary"
+                value={30}
+              />
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <IconButton>
+            <img src="/cleaning.png" alt="Soap"  style={styles.hands}/>
+          </IconButton>
+          <ListItemText
+            disableTypography={true}
+            primary="Household Hygiene"
+          />
+        </ListItem>
+        <ListItem>
+          <IconButton>
+            <img src="/relationship.png" alt="Soap"  style={styles.hands}/>
+          </IconButton>
+          <ListItemText
+            disableTypography={true}
+            primary="Personal contact"
+          />
+        </ListItem>
+        <ListItem>
+          <IconButton>
+            <img src="/brain.png" alt="Soap"  style={styles.hands}/>
+          </IconButton>
+          <ListItemText
+            disableTypography={true}
+            primary="Stay motivated"
+          />
+        </ListItem>
+        <ListItem>
+          <IconButton>
+            <img src="/care.png" alt="Soap"  style={styles.hands}/>
+          </IconButton>
+          <ListItemText
+            disableTypography={true}
+            primary="Self Care"
+          />
+        </ListItem>
+      </List>
+      </div>
     );
   }
 }
