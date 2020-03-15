744587468b0465a7b419bc3be534cfe4c5c24a5a
Igor Nehoroshev <mail@neigor.me>
2020-03-15 13:01:05 +0200

    Create draft of News page

--- web/src/pages/News.tsx
+++ web/src/pages/News.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { Paper } from '@material-ui/core';
+
+const styles = {
+  page: {
+    display: "flex",
+    flexFlow: "column",
+    height: "100%",
+    paddingBottom: "100px"
+  } as CSSProperties,
+  title: {
+    textAlign: "center",
+    fontFamily: "Lato",
+    fontWeight: 700,
+    fontSize: '32px',
+    color: '#262642',
+    marginTop: '40px',
+    marginBottom: '20px',
+  } as CSSProperties,
+  statsPaper: {
+    width: "250px",
+    height: "120px",
+    padding: "20px",
+    marginLeft: "50%",
+    transform: "translateX(-50%)",
+    textAlign: "center"
+  } as CSSProperties,
+  newsPaper: {
+    width: "80%",
+    padding: "20px",
+    marginLeft: "50%",
+    transform: "translateX(-50%)",
+    textAlign: "center",
+    flexGrow : 1
+  } as CSSProperties
+};
 
 export class News extends React.PureComponent {
   render() {
     return (
-      <div>News Page</div>
+      <div style={styles.page}>
+        <Typography variant="h2" style={styles.title}>
+          Recovery Stats
+        </Typography>
+
+        <Paper style={styles.statsPaper}>
+          Stats
+        </Paper>
+
+        <Typography variant="h2" style={styles.title}>
+          Latest News
+        </Typography>
+
+        <Paper style={styles.newsPaper}>
+          News
+        </Paper>
+      </div>
     );
   }
 }
